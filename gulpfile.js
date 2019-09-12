91bec078f8ca97be066714c0c7ca8ab441128228
Roman <romagluten@gmail.com>
2019-09-12 13:39:22 +0300

    подключены нужные шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,34 @@
-const gulp = require("gulp");
-const $ = require("gulp-load-plugins")();
-const browserSync = require("browser-sync");
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const browserSync = require('browser-sync');
 
 const reload = browserSync.reload;
 
 const path = {
   src: {
-    html: "./index.html",
-    scss: "./src/scss/**/*.scss",
-    images: "./src/img/*",
-    js: ["./src/js/script.js"]
+    html: './index.html',
+    scss: './src/scss/**/*.scss',
+    images: './src/img/*',
+    js: ['./src/js/script.js'],
+    fonts: './src/fonts/**'
   },
   build: {
-    root: "./dist",
-    css: "./dist/css",
-    images: "./dist/img",
-    svg: "./dist/img/*.svg",
-    js: "./dist/js"
+    root: './dist',
+    css: './dist/css',
+    images: './dist/img',
+    svg: './dist/img/*.svg',
+    js: './dist/js',
+    fonts: './dist/fonts'
   }
 };
 
-gulp.task("html", () => {
+gulp.task('html', () => {
   return gulp
   .src(path.src.html)
-  .pipe(reload({stream: true}))
+  .pipe(reload({stream: true}));
 });
 
-gulp.task("styles", () => {
+gulp.task('styles', () => {
   return gulp
   .src(path.src.scss)
   .pipe($.plumber())
@@ -45,13 +47,13 @@ gulp.task("styles", () => {
   .pipe(gulp.dest(path.build.css));
 });
 
-gulp.task("scripts", () => {
+gulp.task('scripts', () => {
   return gulp
   .src(path.src.js)
-  .pipe($.concat("main.js"))
+  .pipe($.concat('main.js'))
   .pipe(
       $.babel({
-        presets: ["@babel/env"]
+        presets: ['@babel/env']
       })
   )
   .pipe(
@@ -63,7 +65,7 @@ gulp.task("scripts", () => {
   .pipe(gulp.dest(path.build.js));
 });
 
-gulp.task("images", () => {
+gulp.task('images', () => {
   return gulp
   .src(path.src.images)
   .pipe($.imagemin())
@@ -71,14 +73,14 @@ gulp.task("images", () => {
   .pipe(gulp.dest(path.build.images));
 });
 
-gulp.task("svg-sprite", () => {
+gulp.task('svg-sprite', () => {
   return gulp
   .src(path.build.svg)
   .pipe(
       $.svgSprite({
         mode: {
           symbol: {
-            sprite: "sprite.svg"
+            sprite: 'sprite.svg'
           }
         }
       })
@@ -87,10 +89,10 @@ gulp.task("svg-sprite", () => {
   .pipe(gulp.dest(`${path.build.images}`));
 });
 
-gulp.task("browserSync", () => {
+gulp.task('browserSync', () => {
   browserSync({
     server: {
-      baseDir: "./"
+      baseDir: './'
     },
     port: 1337,
     open: true,
@@ -98,15 +100,21 @@ gulp.task("browserSync", () => {
   });
 });
 
-gulp.task("watch", () => {
-  gulp.watch(path.src.scss, gulp.series("styles"));
-  gulp.watch(path.src.js, gulp.series("scripts"));
-  gulp.watch(path.src.html, gulp.series("html"));
+gulp.task('fonts', () => {
+  return gulp.src(path.src.fonts)
+  .pipe(gulp.dest(path.build.fonts));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(path.src.scss, gulp.series('styles'));
+  gulp.watch(path.src.js, gulp.series('scripts'));
+  gulp.watch(path.src.html, gulp.series('html'));
+  gulp.watch(path.src.fonts, gulp.series('fonts'));
 });
 
 gulp.task(
-    "build",
-    gulp.parallel("styles", "scripts", gulp.series("images", "svg-sprite"))
+    'build',
+    gulp.parallel('styles', 'scripts', 'fonts', gulp.series('images', 'svg-sprite'))
 );
 
-gulp.task("default", gulp.parallel("browserSync", "watch", "styles", "scripts"));
+gulp.task('default', gulp.parallel('browserSync', 'watch', 'styles', 'scripts', 'fonts'));
